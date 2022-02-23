646f9e34e6bd9ad0aa8e03d3ef85f3617f75f659
Robert Edmonds <bobby.edmonds89@gmail.com>
2022-02-22 22:19:17 -0700

    commit tree 860ea852b3722758234a54b05970a7cdf227f0a3

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inItem, setInItem]= useState(false)
+  function addItem(){
+      setInItem((inItem)=> !inItem)
+  }
+  const classChange = inItem ? "in-cart": "";
+  const clickChange = inItem ? "Add to Cart": "Remove From Cart";
   return (
-    <li className="">
+    <li className={classChange}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={addItem}>{clickChange}</button>
     </li>
   );
 }
